4afc60236ca4a4cefa8ab689b02a23fbcbc3ef7c
Jari Bakken <jari.bakken@gmail.com>
2014-08-07 00:01:58 +0200

    Make example clearer.

--- examples/kmediods.rb
+++ examples/kmediods.rb
@@ -1,13 +1,14 @@
 # Example of kmedoids clusterization
 #
 # For more info look at
+# - https://github.com/jarib/rbcluster/blob/master/spec/kmedoids_spec.rb
 # - https://github.com/jarib/rbcluster/blob/master/ext/rbcluster/rbcluster.c#L291
 # - https://github.com/jarib/rbcluster/blob/master/ext/rbcluster/cluster.c#L2729
 # - chapter 8.1.2 page 28 http://bonsai.hgc.jp/~mdehoon/software/cluster/cluster.pdf
 
 require 'rbcluster'
 
-dm = [
+distance_matrix = [
   [],           #
   [12],         # distance between 2nd and 1st
   [76, 5],      # distance between 3rd and 1st, 3rd and 2nd
@@ -15,6 +16,8 @@ dm = [
 ]
 
 # for more info look at https://github.com/jarib/rbcluster/blob/master/ext/rbcluster/rbcluster.c#L291
-result = RbCluster.kmedoids(dm, passes: 100, clusters: 2)
+cluster, error, nfound = RbCluster.kmedoids(distance_matrix, passes: 100, clusters: 2)
 
-p result # => [[0, 1, 1, 1], 15.0, 65]
+p cluster # => [0, 1, 1, 1]
+p error   # => 15.0
+p nfound  # => 65
